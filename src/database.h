fe7fb0a53f81b08707aceba2603626bcfa2cf119
Zahra Bashir <zahra@Nidhis-MBP.hitronhub.home>
2022-04-03 16:16:13 -0600

    Add db.h

--- src/database.h
+++ src/database.h
@@ -7,11 +7,11 @@
 #define OC_W 2
 #define OC_P 3
 #define OC_N 4
+#define VAL_UNK 1000000
+#define DB_MAX_BITS 10
+#define DB_MAX_DOMINANCE_BITS 2
 
-#define DB_MAX_BITS 20
-#define DB_MAX_DOMINANCE_BITS 12
-
-#define DB_ENTRY_SIZE (1 + 2 * sizeof(uint64_t))
+#define DB_ENTRY_SIZE (1 + 2 * sizeof(uint64_t)+ sizeof(uint64_t))
 
 #define DB_GET_OUTCOME(entry) (entry == 0 ? 0 : *((unsigned char *) entry))
 #define DB_SET_OUTCOME(entry, value) *((unsigned char *) entry) = value
@@ -23,7 +23,11 @@
 #define DB_GET_DOMINATED(entry, player) (entry == 0 ? 0 : ((uint64_t *) (&entry[1]))[player - 1])
 #define DB_SET_DOMINATED(entry, player, mask) ((uint64_t *) (&entry[1]))[player - 1] = mask
 
+// #define DB_GET_VALUE(entry) (entry == 0 ? (0,0,0) : (((uint64_t *) (&entry[2]))[0] , ((uint64_t *) (&entry[2]))[1], ((uint64_t *) (&entry[2]))[2]))
+// #define DB_SET_VALUE(entry, value1, value2, value3) (((uint64_t *) (&entry[2]))[0]= value1, ((uint64_t *) (&entry[2]))[1]= value2, ((uint64_t *) (&entry[2]))[2]= value3)
 
+#define DB_GET_VALUE(entry) (entry == 0 ? 0 : ((uint64_t *) (&entry[2]))[0])
+#define DB_SET_VALUE(entry, value) ((uint64_t *) (&entry[2]))[0]= value
 
 class Database {
   private:

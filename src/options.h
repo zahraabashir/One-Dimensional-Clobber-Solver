3fc471f22a335394fa25b35860bda0b9347182e0
tfolkersen <folkerse@ualberta.ca>
2022-04-10 08:06:50 -0600

    options

--- src/options.h
+++ src/options.h
@@ -1,26 +1,26 @@
 #pragma once
 
-#define FIXED_BOARD_SIZE 70
+#define FIXED_BOARD_SIZE 70 //TODO
 
 
 // allow replacement of games with simpler games (games having smaller move counts)
 #define SOLVER_SUBSTITUTE
 
 // allow alpha beta pruning
-#define SOLVER_ALPHA_BETA
+#define SOLVER_ALPHA_BETA //TODO
 
 // don't use variable board sizes -- fix board sizes to FIXED_BOARD_SIZE
-#define SOLVER_FIX_MEMORY_LEAK
+#define SOLVER_FIX_MEMORY_LEAK //TODO
 
 // ignore dominated moves
-#define SOLVER_DELETE_DOMINATED_MOVES
+#define SOLVER_DELETE_DOMINATED_MOVES //TODO
 
 // try searching on a board after deleting a subgame whose outcome agrees with the current player
-#define SOLVER_DELETE_SUBGAMES
+#define SOLVER_DELETE_SUBGAMES //TODO
 
 // 0: sum of black and white moves, 1: sum of black and white undominated moves, 2: length (in tiles)
 // This requires rebuilding the database
-#define SUBGAME_COMPLEXITY_METRIC 0
+#define SUBGAME_COMPLEXITY_METRIC 0 //TODO
 
 
 
@@ -34,3 +34,5 @@
 #define DB_MAX_BOUND_BITS 16
 
 // games are only substituted if they are in the database, and have both dominance and bound information (so the minimum of these will determine what games can be substituted)
+
+

a2225c68427266d5d4ce9ea55052861cb88fd85a
tfolkersen <folkerse@ualberta.ca>
2022-08-09 06:39:02 -0600

    start refactor

--- src/dbtest.cpp
+++ src/dbtest.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
-#include <vector>
-
-#include "database2.h"
-#include "utils.h"
-#include "state.h"
 
 using namespace std;
 
 
 int main() {
-    #include "macro1.cpp"
 
 
     return 0;

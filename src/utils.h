e5f43322d694abe7017a9fbe9a1e661df03ea023
tfolkersen <folkerse@ualberta.ca>
2022-11-18 03:00:06 -0700

    memes

--- src/utils.h
+++ src/utils.h
@@ -14,6 +14,15 @@
 #define B 1
 #define W 2
 
+#define DEBUG_PRINT
+
+#ifdef DEBUG_PRINT
+#define DBOUT( x ) x
+#else
+#define DBOUT( x )
+#endif
+
+
 //Return number of opposing player (1 --> 2), (2 --> 1)
 inline int opponentNumber(int n) { //Inline means this needs to be in the header
     if (n == 1) {
@@ -127,3 +136,5 @@ void makeGame(const std::vector<int> &shape, uint32_t gameNumber,
 
 uint8_t *addGames(uint8_t *g1, size_t g1Size,
     uint8_t *g2, size_t g2Size, size_t *newSize);
+
+

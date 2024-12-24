cc97ec4b195a654a31139fe3f587ad19a6ed244b
tfolkersen <folkerse@ualberta.ca>
2024-12-24 15:43:54 -0700

    add --persist flag

---
+++ src/instructorSolution/runTestsPersistent.py
@@ -0,0 +1,197 @@
+import subprocess
+import atexit
+import time
+import os
+import sys
+import json
+
+testStats = {"totalTime": 0, "totalTests": 0, "correct": 0, "failed": 0, "wrongMove": 0}
+
+proc = subprocess.Popen(["./TheSolvers", "--persist"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1, text=True)
+
+
+fastMode = False
+noColor = False
+
+for f in ["-fast", "-f", "--fast", "--f"]:
+    if f in sys.argv:
+        fastMode = True
+        break
+
+for f in ["-nc", "--nc", "-nocolor", "--nocolor", "-noColor", "--noColor"]:
+    if f in sys.argv:
+        noColor = True
+        break
+
+print("Fast mode (not checking moves)" if fastMode else "Normal mode (checking moves)")
+
+baseDir = os.path.dirname(__file__)
+
+cRed = "\u001b[31m"
+cGreen = "\u001b[32m"
+cYellow = "\u001b[93m"
+cReset = "\u001b[0m"
+
+if noColor:
+    cRed = ""
+    cGreen = ""
+    cYellow = ""
+    cReset = ""
+
+def exitHandler():
+    print(cReset,end="")
+    print(testStats)
+
+    js = json.dumps(cache)
+    cacheFile = open(baseDir + "/cachedOutcomes.json", "w")
+    cacheFile.write(js)
+    cacheFile.close()
+
+
+atexit.register(exitHandler)
+
+
+testList = open(baseDir + "/testlist.txt", "r")
+testFiles = [baseDir + "/tests/" + x.strip() for x in testList if x.strip().find("#") != 0 and x.strip() != ""]
+testList.close()
+
+cacheFile = open(baseDir + "/cachedOutcomes.json", "r")
+cache = json.loads("\n".join([x for x in cacheFile]))
+cacheFile.close()
+
+instructorClobber = baseDir + "/sample_c++/code/clobber"
+
+print("Test files (%d):" % (len(testFiles),))
+for x in testFiles:
+    print(x)
+print("")
+
+def runTest(inputLine, outputLine):
+    testStats["totalTests"] += 1
+
+    board, toPlay, timeLimit = inputLine.split()
+    expectedWinner, expectedMove = outputLine.split()[1 : 3]
+
+    command = "./TheSolvers %s %s %s" % (board, toPlay, "100") 
+
+    start = time.clock_gettime(time.CLOCK_MONOTONIC)
+    proc.stdin.write("%s %s\n" % (board, toPlay))
+    proc.stdin.flush()
+    #result = subprocess.run(command, capture_output = True, shell = True)
+    end = time.clock_gettime(time.CLOCK_MONOTONIC)
+    output = proc.stdout.readline().rstrip("\n")
+
+    testTime = end - start
+    testStats["totalTime"] += testTime
+
+    splitOutput = output.split()
+    outcome = splitOutput[0]
+    move = splitOutput[1]
+    reportedTime = 0
+    nodes = 1
+
+    #print(command)
+    #print(output)
+
+    correctness = 0
+    #-1 -- fail, 0 -- bad move, 1 -- correct
+
+    if fastMode:
+        if outcome != expectedWinner:
+            correctness = -1
+            testStats["failed"] += 1
+        else:
+            correctness = 1
+            testStats["correct"] += 1
+
+    if not fastMode:
+        if outcome != expectedWinner:
+            correctness = -1
+            testStats["failed"] += 1
+        else:
+            if move == expectedMove:
+                correctness = 1
+                testStats["correct"] += 1
+            else:
+                if "None" in [move, expectedMove]:
+                    correctness = 0
+                    testStats["wrongMove"] += 1
+                else:
+                    newBoard = [x for x in board]
+                    a, b = [int(x) for x in move.split("-")]
+
+                    #print(str(a) + " " + str(b))
+                    #print(len(newBoard))
+
+                    if newBoard[a] not in ["B", "W"] or newBoard[b] not in ["B", "W"] or newBoard[a] == newBoard[b]:
+                        #invalid move
+                        correctness = 0
+                        testStats["wrongMove"] += 1
+                    else:
+                        newBoard[b] = newBoard[a]
+                        newBoard[a] = "."
+                        newBoard = "".join(newBoard)
+                        newBoard = [x for x in newBoard.split(".") if len(x) > 1]
+                        newBoard = ".".join(newBoard)
+
+                        nextPlayer = "W" if toPlay == "B" else "B"
+
+                        outcome2 = None
+
+                        if not (newBoard.find("BW") == -1 and newBoard.find("WB") == -1):
+                            key = newBoard + " " + nextPlayer
+                            if key in cache.keys():
+                                outcome2 = cache[key]
+                            else:
+                                command2 = instructorClobber + " %s %s %s" % (newBoard, nextPlayer, "100000")
+                                result2 = subprocess.run(command2, capture_output = True, shell = True)
+                                output2 = result2.stdout.decode("utf-8").rstrip("\n")
+                                outcome2 = output2.split()[0]
+                                cache[key] = outcome2
+                        else:
+                            outcome2 = toPlay
+
+                        if outcome2 == expectedWinner:
+                            correctness = 1
+                            testStats["correct"] += 1
+                        else:
+                            correctness = 0
+                            testStats["wrongMove"] += 1
+
+                        
+
+
+    print([cRed, cYellow, cGreen][correctness + 1], end="")
+    print(command)
+    print("OUT: " + output)
+    print("EXP: " + " ".join(outputLine.split()[1 : ]))
+    print(["[FAIL]", "[WRONG MOVE]", "[PASS]"][correctness + 1])
+    print(cReset)
+
+
+for f in testFiles:
+    file = open(f, "r")
+
+    line1 = ""
+    line2 = ""
+
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        line = line.strip()
+
+        line2 = line
+
+        #Run test
+        if line2.find("#?") == 0:
+            print(f)
+            runTest(line1, line2)
+
+        line1 = line2
+
+
+
+    file.close()
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -55,16 +55,119 @@ bool test() {
     return true;
 }
 
+void printUsage(const char *execName) {
+    cout << "Usage:\n" << execName << " [--persist] <board> <toPlay> <time (ignored)>" << endl;
+}
+
+void persistMain() {
+    Database db;
+    db.load();
+
+
+    string boardStr;
+    string toPlayStr;
+
+    //int rootPlayer = charToPlayerNumber(*argv[2]);
+
+    Solver solver(70, &db); // boardLen is only used to pick TT hash length
+
+    while (cin) {
+        cin >> boardStr;
+
+        if (cin.fail()) {
+            if (!cin.eof()) {
+                cout << "Some input error happened reading board" << endl;
+            }
+            return;
+        }
+
+        cin >> toPlayStr;
+
+        if (cin.fail()) {
+            if (!cin.eof()) {
+                cout << "Some input error happened reading player" << endl;
+            }
+            return;
+        }
+
+        // Validate input
+        for (int i = 0; i < boardStr.size(); i++) {
+            const char &c = boardStr[i];
+            if (c != '.' && c != 'B' && c != 'W') {
+                cout << "Invalid board character. Use 'B', 'W', and '.'" << endl;
+                return;
+            }
+        }
+
+        if (toPlayStr.size() != 1 || (toPlayStr[0] != 'B' && toPlayStr[0] != 'W')) {
+            cout << "Bad player character. Use 'B' or 'W'" << endl;
+            return;
+        }
+
+        size_t boardLen = strlen(boardStr.c_str());
+        uint8_t board[boardLen];
+
+        memcpy(board, boardStr.c_str(), boardLen);
+
+        // sanity checks
+        if (boardLen != boardStr.size()) { // string size() excludes null
+            cout << "Wrong board size" << endl;
+            return;
+        }
+
+        for (size_t i = 0; i < boardLen; i++) {
+            if (board[i] != boardStr[i]) {
+                cout << "Bad board copy" << endl;
+                return;
+            }
+        }
+
+        // Convert board format
+        for (size_t i = 0; i < boardLen; i++) {
+            board[i] = charToPlayerNumber(board[i]);
+        }
+
+        int rootPlayer = charToPlayerNumber(toPlayStr[0]);
+
+        // Reset these
+        node_count = 0;
+        best_from = -1;
+        best_to = -1;
+
+        int result = solver.solveID(board, boardLen, rootPlayer);
+
+        if (best_from == -1) {
+            cout << playerNumberToChar(result) << " None" << " " << node_count;
+        } else {
+            cout << playerNumberToChar(result) << " " << best_from << "-" << best_to << " " << node_count;
+        }
+
+        cout << endl;
+    }
+}
+
 
 int main(int argc, char **argv) {
     if (test()) {
         return 0;
     }
 
-    if (argc < 3) {
-        cout << "Usage:\n" << argv[0] << " <board> <toPlay> <time (ignored)>" << endl;
+    if (argc < 2) {
+        printUsage(argv[0]);
         return 0;
     }
+
+    bool persist = strcmp(argv[1], "--persist") == 0;
+    if (persist) {
+        persistMain();
+        return 0;
+    }
+
+    if (argc < 3) {
+        printUsage(argv[0]);
+        return 0;
+    }
+
 
     Database db;
     db.load();
